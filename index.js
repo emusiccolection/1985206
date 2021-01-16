e79a8658fe6b0a70c2c90b44c3bfa3b29771b917
emusiccolection <63217419+emusiccolection@users.noreply.github.com>
2021-01-16 09:57:43 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const { request } = require("express");
+
+let express = require('express');
+let app = express();
+
+// Public Static Folder
+app.use(express.static(__dirname + '/public'));
+
+// Use View Engine
+let expressHbs = require('express-handlebars');
+let hbs = expressHbs.create({
+    extname: 'hbs',
+    defaultLayout: 'layout',
+    layoutsDir: __dirname + '/views/layouts/',
+    partialsDir: __dirname + '/views/partials'
+});
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+
+//Define your router here
+app.get('/', (req, res) => {
+    res.render('index');
+});
+app.get('/:page', (req, res) => {
+    let banners = {
+        blog: 'Our Blog',
+        category: 'Category'
+    };
+    let page = req.params.page;
+    res.render(page, {banner: banners[page]});
+});
+
+// Server Port & Start Server
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), () => {
+    console.log(`Server is running at port ${app.get('port')}`);
+});
